18b67f156ea313e970e19ab78d213e9ceac852b0
washswat-peace <syneffort@gmail.com>
2021-08-12 11:33:53 +0900

    task queue

---
+++ 04.asyncByCallback/01.webSpider/SpiderV4/spider-cli.js
@@ -0,0 +1,12 @@
+const spider = require('./spiderV4');
+const TaskQueue = require('../../../00.core/parallelPattern/TaskEventQueue');
+
+const url = process.argv[2];
+const nesting = Number.parseInt(process.argv[3], 10) || 1;
+const concurrency = Number.parseInt(process.argv[4], 10) || 2;
+
+const spiderQueue = new TaskQueue(concurrency);
+spiderQueue.on('error', console.error);
+spiderQueue.on('empty', () => console.log('Download complete'));
+
+spider(url, nesting, spiderQueue);
---
+++ 04.asyncByCallback/01.webSpider/SpiderV4/spiderV4.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const path = require('path');
+const superagent = require('superagent');
+const mkdirp = require('mkdirp');
+const { urlToFilename, getPageLinks } = require('../utils');
+
+function saveFile(filename, contents, cb) {
+    mkdirp(path.dirname(filename), err => {
+        if (err) {
+            return cb(err);
+        }
+
+        fs.writeFile(filename, contents, cb);
+    });
+}
+
+function download(url, filename, cb) {
+    console.log(`Downloading ${url}`);
+    superagent.get(url).end((err, res) => {
+        if (err) {
+            return cb(err);
+        }
+
+        saveFile(filename, res.text, err => {
+            if (err) {
+                return cb(err);
+            }
+
+            console.log(`Downloaded and saved: ${url}`);
+            cb(null, res.text);
+        });
+    });
+}
+
+function spiderLinks(currentUrl, body, nesting, queue) {
+    if (nesting === 0) {
+        return;
+    }
+
+    const links = getPageLinks(currentUrl, body);
+    if (links.length === 0) {
+        return;
+    }
+
+    links.forEach(link => spider(link, nesting - 1, queue));
+}
+
+function spiderTask(url, nesting, queue, cb) {
+    const filename = urlToFilename(url);
+    fs.readFile(filename, 'utf8', (err, fileContent) => {
+        if (err) {
+            if (err.code !== 'ENOENT') {
+                return cb(err);
+            }
+
+            return download(url, filename, (err, requestContent) => {
+                if (err) {
+                    return cb(err);
+                }
+
+                spiderLinks(url, requestContent, nesting, queue);
+                return cb();
+            });
+        }
+    });
+}
+
+const spidering = new Set();
+function spider(url, nesting, queue) {
+    if (spidering.has(url)) {
+        return;
+    }
+
+    spidering.add(url);
+    queue.pushTask((done) => {
+        spiderTask(url, nesting, queue, done);
+    });
+}
+
+module.exports = spider;
